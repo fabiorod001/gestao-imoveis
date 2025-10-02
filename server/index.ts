a04e03dc6ff42226b781afa0b446ca18365c5b17
Fabio Rodrigues <fabiorod001@gmail.com>
2025-10-02 18:11:05 +0000

    Add Vercel domain to CORS whitelist

--- server/index.ts
+++ server/index.ts
@@ -38,6 +38,7 @@ app.use(cors({
     'http://localhost:5173',
     'http://localhost:5000', 
     'https://quiet-pudding-f152f9.netlify.app'
+    'https://gestao-imoveis-pi.vercel.app'
   ],
   credentials: true
 }));
